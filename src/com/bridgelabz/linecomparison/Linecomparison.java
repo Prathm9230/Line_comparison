93a910fd789e87a5fe7cc7d6be042a8965bbfd0c
Prathm <jawahireprathmesh@gmail.com>
2023-06-21 10:24:24 +0530

    Welcome To Line comparison Program Computation.

---
+++ src/com/bridgelabz/linecomparison/Linecomparison.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.linecomparison;
+import java.util.Scanner;
+
+public class Linecomparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line comaprison problems.");
+    }
+}
