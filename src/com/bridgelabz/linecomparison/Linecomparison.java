f6e68f007d24d1c78ad9a8f9cfac56a1faa697d4
Prathm <jawahireprathmesh@gmail.com>
2023-06-21 11:36:34 +0530

    To check equality of two lines

--- src/com/bridgelabz/linecomparison/Linecomparison.java
+++ src/com/bridgelabz/linecomparison/Linecomparison.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Linecomparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line comaprison problems.");
+        /*System.out.println("Welcome to Line comaprison problems.");
         //UC1:Calculate length of line.
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of x1:");
@@ -16,8 +16,39 @@ public class Linecomparison {
         float y2 = sc.nextInt();
 
         double Length =  Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
-        System.out.println("The length of line is:"+Length);
+        System.out.println("The length of line is:"+Length);*/
 
+        //uC-2:To check equality of two lines.
+        double l1,l2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of x1:");
+        int x1 = sc.nextInt();
+        System.out.println("Enter the value of y1:");
+        int y1 = sc.nextInt();
+        System.out.println("Enter the value of x2:");
+        int x2 = sc.nextInt();
+        System.out.println("Enter the value of y2:");
+        
+        int y2 = sc.nextInt();
+        System.out.println("Enter the value of a1:");
+        int a1 = sc.nextInt();
+        System.out.println("Enter the value of b1:");
+        int b1 = sc.nextInt();
+        System.out.println("Enter the value of a2:");
+        int a2 = sc.nextInt();
+        System.out.println("Enter the value of b2:");
+        int b2 = sc.nextInt();
 
+        l1 =  Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+        l2 =  Math.sqrt(Math.pow((a2-a1),2) + Math.pow((b2-b1),2));
+
+        if(l1 == l2)
+        {
+            System.out.println("Line l1 equals to Line l2.");
+        }
+        else
+        {
+            System.out.println("Line l1 is not equals to Line l2");
+        }
     }
 }
